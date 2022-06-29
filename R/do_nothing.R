3072c20761cae50ba6915fb6985db2e2c980c8f6
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-29 14:59:19 -0700

    🔥 Remove unused test

--- R/do_nothing.R
+++
@@ -1,3 +0,0 @@
-return_one <- function() {
-  return(1)
-}
--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,11 +1,3 @@
-describe("Test all is ready", {
-  it("Return one", {
-    expected <- 1
-    obtained <- return_one()
-    expect_equal(expected, obtained)
-  })
-})
-
 describe("Get version of the module", {
   it("The version is 0.1.0", {
     expected_version <- c("0.1.0")
