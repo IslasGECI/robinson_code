10f1565f32a4a2017a1b331315a3260159bbba82
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-29 15:08:21 -0700

    🎨 Make format

--- R/change_from_tidy_2_data_structures.R
+++ R/change_from_tidy_2_data_structures.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 
 
-f <- function(data){
-    result <- tibble(
+f <- function(data) {
+  result <- tibble(
     Grid_ID = c(1, 2, 1, 2),
-        Session = c(1, 1, 2, 2),
-        r1 = c(),
-        r2 = c(),
-        r3 = c(),
-        r4 = c(),
-        e1 = c(),
-        e2 = c(),
-        e3 = c(),
-        e4 = c()
-    )
-    return(result)
+    Session = c(1, 1, 2, 2),
+    r1 = c(),
+    r2 = c(),
+    r3 = c(),
+    r4 = c(),
+    e1 = c(),
+    e2 = c(),
+    e3 = c(),
+    e4 = c()
+  )
+  return(result)
 }
--- tests/testthat/test_change_from_tidy_2_data_structures.R
+++ tests/testthat/test_change_from_tidy_2_data_structures.R
@@ -1,26 +1,26 @@
 library(tidyverse)
 
 describe("We define the expected data structures", {
-    it("Expected data structure", {
-        path <- "../data/raw_cameras.csv"
-        data <- read_csv(path)
-        expected <- tibble(
-            Grid_ID = c(1, 2, 1, 2),
-            Session = c(1, 1, 2, 2),
-            r1 = c(),
-            r2 = c(),
-            r3 = c(),
-            r4 = c(),
-            e1 = c(),
-            e2 = c(),
-            e3 = c(),
-            e4 = c()
-        )
-        obtained <- f(data)
-        expect_equal(expected, obtained)
-    })
-    it("Read raw_cameras data",{
-        path <- "../data/raw_cameras.csv"
-        data <- read_csv(path)
-    })
+  it("Expected data structure", {
+    path <- "../data/raw_cameras.csv"
+    data <- read_csv(path)
+    expected <- tibble(
+      Grid_ID = c(1, 2, 1, 2),
+      Session = c(1, 1, 2, 2),
+      r1 = c(),
+      r2 = c(),
+      r3 = c(),
+      r4 = c(),
+      e1 = c(),
+      e2 = c(),
+      e3 = c(),
+      e4 = c()
+    )
+    obtained <- f(data)
+    expect_equal(expected, obtained)
+  })
+  it("Read raw_cameras data", {
+    path <- "../data/raw_cameras.csv"
+    data <- read_csv(path)
+  })
 })
