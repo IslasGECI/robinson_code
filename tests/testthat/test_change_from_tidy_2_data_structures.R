d931bc3c0c438403122ec274fe7963adcac27ab2
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-29 15:29:56 -0700

    🧪Add ftest with detection

--- tests/testthat/test_change_from_tidy_2_data_structures.R
+++ tests/testthat/test_change_from_tidy_2_data_structures.R
@@ -19,8 +19,22 @@ describe("We define the expected data structures", {
     obtained <- f(data)
     expect_equal(expected, obtained)
   })
-  it("Read raw_cameras data", {
-    path <- "../data/raw_cameras.csv"
+  it("Read camaras with a detection", {
+    path <- "../data/raw_cameras_with_detection.csv"
     data <- read_csv(path)
+    expected <- tibble(
+      Grid_ID = c(1),
+      Session = c(1),
+      r1 = c(1),
+      r2 = c(),
+      r3 = c(),
+      r4 = c(),
+      e1 = c(3),
+      e2 = c(),
+      e3 = c(),
+      e4 = c()
+    )
+    obtained <- f(data)
+    expect_equal(expected, obtained)
   })
 })
