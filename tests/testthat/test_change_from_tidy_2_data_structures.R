2b4a634289393339e309010752596c9a3db34b1f
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-29 14:36:31 -0700

    🧪 Add failing tesst

---
+++ tests/testthat/test_change_from_tidy_2_data_structures.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+describe("We define the expected data structures", {
+    it("Expected data structure", {
+        expected <- tibble(
+            Grid_ID = c(1, 2, 1, 2),
+            Session = c(1, 1, 2, 2),
+            r1 = c(),
+            r2 = c(),
+            r3 = c(),
+            r4 = c(),
+            e1 = c(),
+            e2 = c(),
+            e3 = c(),
+            e4 = c()
+        )
+        obtained <- f()
+        expect_equal(expected, obtained)
+    })
+})
