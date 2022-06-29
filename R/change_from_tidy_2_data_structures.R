5d751934afc64dca9af8689436d077b02978e554
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-29 15:24:44 -0700

    ✅ Pass test

--- R/change_from_tidy_2_data_structures.R
+++ R/change_from_tidy_2_data_structures.R
@@ -3,13 +3,13 @@ library(tidyverse)
 
 f <- function(data) {
   result <- tibble(
-    Grid_ID = c(1, 2, 1, 2),
-    Session = c(1, 1, 2, 2),
-    r1 = c(),
+    Grid_ID = c(1),
+    Session = c(1),
+    r1 = c(0),
     r2 = c(),
     r3 = c(),
     r4 = c(),
-    e1 = c(),
+    e1 = c(3),
     e2 = c(),
     e3 = c(),
     e4 = c()
