e0ca0e254667f5e4fe122149fea2d5a02b49884f
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-11 10:11:07 -0700

    🎨 Make format

--- R/change_from_raw_2_tidy_structures.R
+++ R/change_from_raw_2_tidy_structures.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
-raw_2_tidy <- function(data){
-    result <- tibble(
-        date = c(),
-        Grid_ID = c(),
-        r = c(),
-        e = c()
-    )
-    return(result)
+raw_2_tidy <- function(data) {
+  result <- tibble(
+    date = c(),
+    Grid_ID = c(),
+    r = c(),
+    e = c()
+  )
+  return(result)
 }
--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
 describe("Define tidy data structure", {
-    it("Expected tidy data structure", {
-        path <- "../data/raw_cameras.csv"
-        data <- read_csv(path)
-        expected_tidy <- tibble(
-            date = c(),
-            Grid_ID = c(),
-            r = c(),
-            e = c()
-        )
-        obtained_tidy <- raw_2_tidy(data)
-        expect_equal(expected_tidy, obtained_tidy)
-    })
+  it("Expected tidy data structure", {
+    path <- "../data/raw_cameras.csv"
+    data <- read_csv(path)
+    expected_tidy <- tibble(
+      date = c(),
+      Grid_ID = c(),
+      r = c(),
+      e = c()
+    )
+    obtained_tidy <- raw_2_tidy(data)
+    expect_equal(expected_tidy, obtained_tidy)
+  })
 })
