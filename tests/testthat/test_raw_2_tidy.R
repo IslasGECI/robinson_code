ece04ab484d28376071305317c9973a2d14c7c5a
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-11 09:47:45 -0700

    🧪 Add failing test

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -2,6 +2,8 @@ library(tidyverse)
 
 describe("Define tidy data structure", {
     it("Expected tidy data structure", {
-        raw_2_tidy()
+        path <- "../data/raw_cameras.csv"
+        data <- read_csv(path)
+        raw_2_tidy(data)
     })
 })
