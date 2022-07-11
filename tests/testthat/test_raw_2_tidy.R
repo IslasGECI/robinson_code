aa2233b76b57081db1c3ad3fa6804dc1cc5fdb71
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-11 11:37:36 -0700

    🔥 Remove code

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -14,12 +14,4 @@ describe("Define tidy data structure", {
     obtained_tidy <- raw_2_tidy(data)
     expect_equal(expected_tidy, obtained_tidy)
   })
-  it("Expected date format", {
-#    path <- "../data/raw_cameras.csv"
-#    data <- read_csv(path)
-#    obtained_tidy <- raw_2_tidy(data)
-#    obtained_date <- obtained_tidy$date[1]
-#    expected_date <- "2022-04-02 07:25:23"
-#    expect_equal(expected_date,obtained_date)
-  })
 })
