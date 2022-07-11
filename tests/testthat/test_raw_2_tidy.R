8de7d3cd338f4e5c5ee40962cf10ac6fdd963721
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-11 11:41:41 -0700

    🧪 Add dates

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -4,9 +4,16 @@ describe("Define tidy data structure", {
   it("Expected tidy data structure", {
     path <- "../data/raw_cameras.csv"
     data <- read_csv(path)
-    expected_date <- "2022-04-02 07:25:23"
+    expected_dates <- c(
+      "2022-04-02 07:25:23",
+      "2022-04-02 07:25:26",
+      "2022-04-02 07:25:29",
+      "2022-04-02 07:25:32",
+      "2022-04-04 02:02:42",
+      "2022-04-04 02:02:45"
+    )
     expected_tidy <- tibble(
-      date = c(expected_date),
+      date = c(expected_dates),
       Grid_ID = c(),
       r = c(),
       e = c()
