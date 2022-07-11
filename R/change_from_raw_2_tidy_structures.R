be69b451a9a58f590808589e581d5a05c5ed7722
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-11 14:19:40 -0700

    ✅ Pass test

--- R/change_from_raw_2_tidy_structures.R
+++ R/change_from_raw_2_tidy_structures.R
@@ -3,10 +3,11 @@ library(tidyverse)
 raw_2_ocassion <- function(data) {
   dates <- as.character(data$DateTime)
   ocassions <- lubridate::week(dates)
+  camera_IDs <- as.numeric(gsub(".*?([0-9]+).*", "\\1", data$RelativePath))
   result <- tibble(
     date = dates,
     ocassion = ocassions,
-    camera_ID = c(),
+    camera_ID = camera_IDs,
     coati_count = c()
   )
   return(result)
