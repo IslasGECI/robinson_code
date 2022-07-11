0609be8c560459aa7bfd009e513de5ba1dd4bcb4
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-11 09:56:27 -0700

    🧪 Add tidy structure

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -4,6 +4,13 @@ describe("Define tidy data structure", {
     it("Expected tidy data structure", {
         path <- "../data/raw_cameras.csv"
         data <- read_csv(path)
-        raw_2_tidy(data)
+        expected_tidy <- tibble(
+            date = c(),
+            Grid_ID = c(),
+            r = c(),
+            e = c()
+        )
+        obtained_tidy <- raw_2_tidy(data)
+        expect_equal(expected_tidy, obtained_tidy)
     })
 })
