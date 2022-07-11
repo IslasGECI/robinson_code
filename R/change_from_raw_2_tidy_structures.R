0e5cdaae0c8fa02878de9ad59834021246067172
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-11 12:59:56 -0700

    ♻️ Rename variables

--- R/change_from_raw_2_tidy_structures.R
+++ R/change_from_raw_2_tidy_structures.R
@@ -2,10 +2,10 @@ library(tidyverse)
 
 raw_2_ocassion <- function(data) {
   dates <- as.character(data$DateTime)
-  get_ocassion <- lubridate::week(dates)
+  ocassions <- lubridate::week(dates)
   result <- tibble(
-    date = c(dates),
-    ocassion = get_ocassion,
+    date = dates,
+    ocassion = ocassions,
     camera_ID = c(),
     coati_count = c()
   )
