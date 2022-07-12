a48aaf720b1e8bace7ec1561703d5813ab35d32a
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-12 11:35:32 -0700

    🧪 Add new date

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -27,15 +27,15 @@ describe("Define ocassion data structure", {
 })
 describe("Define ocassion data structure", {
   it("Expected tidy data structure", {
-    path <- "../data/raw_cameras.csv"
+    path <- "../data/raw_cameras_effort.csv"
     data <- read_csv(path)
     ocassion_structure <- raw_2_ocassion(data)
     obtained_tidy <- ocassion_2_tidy(ocassion_structure)
-    expected_id <- c(1, 2 ,10)
-    expected_ocassion <- c(13,14,14)
-    expected_captures <- c(1,0,1)
-    expected_method <- c("Camera-Traps", "Camera-Traps", "Camera-Traps")
-    expected_effort <- c(1,1,1)
+    expected_id <- c(1, 2 ,10, 10)
+    expected_ocassion <- c(13,14,14,14)
+    expected_captures <- c(1,0,1,0)
+    expected_method <- c("Camera-Traps", "Camera-Traps", "Camera-Traps","Camera-Traps")
+    expected_effort <- c(1,1,1,1)
     expected_tidy <- tibble(
       camera_id = expected_id,
       ocassion = expected_ocassion,
