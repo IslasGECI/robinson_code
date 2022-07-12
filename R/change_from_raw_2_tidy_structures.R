a917785ae946e512c15dccdabe427a890a721acc
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-12 14:15:06 -0700

    ✅ Pass test

--- R/change_from_raw_2_tidy_structures.R
+++ R/change_from_raw_2_tidy_structures.R
@@ -21,23 +21,7 @@ group_filtered_data <- function(ocassion_structure) {
       group_by(camera_id, ocassion, day = lubridate::day(date)) %>%
       summarize(r = sum(coati_count)) %>%
       mutate(e = 1, method = "Camera-Traps") 
-  n_rows = nrow(filtered_structure)
-  tidy_structure <- tibble(
-      camera_id = filtered_structure$camera_id,
-      ocassion = filtered_structure$ocassion,
-      r = filtered_structure$r,
-      e = rep(1, n_rows),
-      method = rep("Camera-Traps",n_rows)
-    )
-  return(tidy_structure)
-}
-
-xxocassion_2_tidy <- function(ocassion_structure) {
-  filtered_structure <- ocassion_structure %>%
-      group_by(camera_id, ocassion, day = lubridate::day(date)) %>%
-      summarize(r = sum(coati_count)) %>%
-      mutate(e = 1, method = "Camera-Traps") 
-  return(filtered_structure)
+  return(as_tibble(filtered_structure))
 }
 calculate_effort <- function(raw_table) {
   tidy_table <- group_filtered_data(raw_table)
