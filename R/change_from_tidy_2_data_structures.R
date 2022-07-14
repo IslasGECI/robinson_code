f544242fe904949e3cbc209245483ff1345f94f1
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-07-14 15:33:07 -0700

    🔥 Remove unused function

--- R/change_from_tidy_2_data_structures.R
+++ R/change_from_tidy_2_data_structures.R
@@ -1,23 +1,6 @@
 library(tidyverse)
 
 
-get_cameras_data_structure <- function(data) {
-  coati_count <- sum(data$CoatiCount)
-  result <- tibble(
-    Grid_ID = c(1),
-    Session = c("April"),
-    r1 = c(coati_count),
-    r2 = c(),
-    r3 = c(),
-    r4 = c(),
-    e1 = c(3),
-    e2 = c(),
-    e3 = c(),
-    e4 = c()
-  )
-  return(result)
-}
-
 #' @export
 tidy_2_final <- function(tidy_table) {
   first_month_week <- min(tidy_table$ocassion)
