fb83b18b53eb04baecd12eee629870abbad2d07e
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-14 10:32:55 -0700

    🗃️  Write csv with final structure

--- src/get_final_data_structure.R
+++ src/get_final_data_structure.R
@@ -3,4 +3,5 @@ library(robinson)
 
 path <- "data/raw/robinson_coati_detection_camera_traps/APRIL2022COATI.csv"
 tidy_table <- tidy_from_path(path)
-print(tidy_table)
+final <- tidy_2_final(tidy_table)
+write_csv(final, file = "data/april_camera_traps.csv")
