c6fa09e3269b3299e45471387bdf53b6f331fdd4
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-22 13:06:31 -0700

    🚧 Working unamed_function()

--- R/change_from_raw_2_tidy_structures.R
+++ R/change_from_raw_2_tidy_structures.R
@@ -8,6 +8,13 @@ get_initial_and_delta_day_by_camera <- function(raw_data){
   return(limit_days_by_camera)
 }
 
+unamed_function <- function(limit_days_by_camera){
+  renglon <- tibble(RelativePath = limit_days_by_camera$RelativePath, 
+  coati_count = 0, DateTime = limit_days_by_camera$initial_day + days(0:limit_days_by_camera$delta_day))
+  for (i in 0:nrow(limit_days_by_camera)){print(limit_days_by_camera$RelativePath[i])}
+  return(renglon)
+}
+
 fill_days <- function(raw_data){
   return(raw_data)
 }
