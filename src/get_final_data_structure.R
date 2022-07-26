e31491c54860215e656fd718bb8fc7cefb2ca883
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-07-26 15:55:40 -0700

    🪲Fix bug

--- src/get_final_data_structure.R
+++ src/get_final_data_structure.R
@@ -4,5 +4,4 @@ library(robinson)
 path <- "data/raw/robinson_coati_detection_camera_traps/APRIL2022COATI.csv"
 tidy_table <- tidy_from_path(path)
 final <- tidy_2_final(tidy_table)
-final <- change_na_by_0(final)
-write_csv(final, file = "data/april_camera_traps.csv", na = 0)
+write_csv(final, file = "data/april_camera_traps.csv", na = "0")
