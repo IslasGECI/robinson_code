8785f920528564e515e6a6bc076771c6a6641b8c
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-27 15:24:33 -0700

    ✅Pass test

--- R/trapping_hunting.R
+++ R/trapping_hunting.R
@@ -5,3 +5,9 @@ get_hunting_effort <- function(raw_trapping_hunting){
       mutate(hunting_effort = Days_on_terrain * Persons_on_terrain)
   return(hunting_effort)
 }
+
+get_ocassion <- function(raw_trapping_hunting){
+  ocassions <- raw_trapping_hunting %>% 
+      mutate(ocassion = lubridate::isoweek(raw_trapping_hunting$Date))
+  return(ocassions)
+}
