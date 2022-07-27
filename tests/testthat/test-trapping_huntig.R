156dcff8c132710986040f2b57aaafa41f540f3d
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-27 15:28:39 -0700

    ♻️ Extract variable

--- tests/testthat/test-trapping_huntig.R
+++ tests/testthat/test-trapping_huntig.R
@@ -1,18 +1,15 @@
 library(tidyverse)
 
 describe("Get tidy structure from trapping and hunting data", {
+  path <- "../data/input_trapping_hunting.csv"
+  raw_data <- read_csv(path)
+  x <- get_hunting_effort(raw_data) %>% get_ocassion()
   it("Get hunting effort", {
-    path <- "../data/input_trapping_hunting.csv"
-    raw_data <- read_csv(path)
-    x <- get_hunting_effort(raw_data)
     obtained_hunting_effort <- x$hunting_effort
     expected_hunting_effort <- c(30, 12, 30, 10, 18, 21)
     expect_equal(obtained_hunting_effort, expected_hunting_effort)
   })
   it("Get ocassion", {
-    path <- "../data/input_trapping_hunting.csv"
-    raw_data <- read_csv(path)
-    x <- get_ocassion(raw_data)
     obtained_ocassion <- x$ocassion
     expected_ocassion <- c(12, 14, 14, 15, 16, 20)
     expect_equal(obtained_ocassion, expected_ocassion)
