e7ff4d6862d09a30af2ea3702457034b3ac91ade
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-27 15:19:32 -0700

    🛑🧪 Set failing test

--- tests/testthat/test-trapping_huntig.R
+++ tests/testthat/test-trapping_huntig.R
@@ -8,5 +8,13 @@ describe("Get tidy structure from trapping and hunting data", {
     obtained_hunting_effort <- x$hunting_effort
     expected_hunting_effort <- c(30, 12, 30, 10, 18, 21)
     expect_equal(obtained_hunting_effort, expected_hunting_effort)
+  }),
+  it("Get ocassion", {
+    path <- "../data/input_trapping_hunting.csv"
+    raw_data <- read_csv(path)
+    x <- get_ocassion(raw_data)
+    obtained_ocassion <- x$ocassion
+    expected_ocassion <- c(12, 14, 14, 15, 17, 21)
+    expect_equal(obtained_ocassion, expected_ocassion)
   })
 })
