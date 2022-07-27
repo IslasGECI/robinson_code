70904e93304175873695148d7fec1c7d96b045a8
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-27 15:48:30 -0700

    🛑🧪 Set failing test

--- tests/testthat/test-trapping_hunting.R
+++ tests/testthat/test-trapping_hunting.R
@@ -3,7 +3,10 @@ library(tidyverse)
 describe("Get tidy structure from trapping and hunting data", {
   path <- "../data/input_trapping_hunting.csv"
   raw_data <- read_csv(path)
-  x <- get_hunting_effort(raw_data) %>% get_ocassion() %>% get_session()
+  x <- get_hunting_effort(raw_data) %>%
+    get_ocassion() %>%
+    get_session() %>%
+    select_columns()
   it("Get hunting effort", {
     obtained_hunting_effort <- x$hunting_effort
     expected_hunting_effort <- c(30, 12, 30, 10, 18, 21)
@@ -18,5 +21,10 @@ describe("Get tidy structure from trapping and hunting data", {
     obtained_session <- x$session
     expected_session <- c(3, 4, 4, 4, 4, 5)
     expect_equal(obtained_session, expected_session)
+  })
+  it("Get needed columns", {
+    obtained_columns <- colnames(x)
+    expected_columns <- c("Date", "Grid", "Days_on_terrain", "Hunted_Coati", "Night-traps", "Captured_Coati", "Observed_Coati", "hunting_effort", "ocassion", "session")
+    expect_equal(obtained_columns, expected_columns)
   })
 })
