99bb7e6ecdbec8c188789f61cde6ee0e7fa66f98
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-27 15:37:16 -0700

    ✅Pass test

--- R/trapping_hunting.R
+++ R/trapping_hunting.R
@@ -1,13 +1,19 @@
 library(tidyverse)
 
-get_hunting_effort <- function(raw_trapping_hunting) {
-  hunting_effort <- raw_trapping_hunting %>%
-    mutate(hunting_effort = Days_on_terrain * Persons_on_terrain)
+get_hunting_effort <- function(raw_trapping_hunting){
+  hunting_effort <- raw_trapping_hunting %>% 
+      mutate(hunting_effort = Days_on_terrain * Persons_on_terrain)
   return(hunting_effort)
 }
 
-get_ocassion <- function(raw_trapping_hunting) {
-  ocassions <- raw_trapping_hunting %>%
-    mutate(ocassion = lubridate::isoweek(raw_trapping_hunting$Date))
+get_ocassion <- function(raw_trapping_hunting){
+  ocassions <- raw_trapping_hunting %>% 
+      mutate(ocassion = lubridate::isoweek(raw_trapping_hunting$Date))
   return(ocassions)
+}
+
+get_session <- function(data_trapping_hunting){
+  sessions <- data_trapping_hunting %>% 
+      mutate(session = lubridate::month(data_trapping_hunting$Date))
+  return(sessions)
 }
--- tests/testthat/test-trapping_hunting.R
+++ tests/testthat/test-trapping_hunting.R
@@ -3,7 +3,7 @@ library(tidyverse)
 describe("Get tidy structure from trapping and hunting data", {
   path <- "../data/input_trapping_hunting.csv"
   raw_data <- read_csv(path)
-  x <- get_hunting_effort(raw_data) %>% get_ocassion()
+  x <- get_hunting_effort(raw_data) %>% get_ocassion() %>% get_session()
   it("Get hunting effort", {
     obtained_hunting_effort <- x$hunting_effort
     expected_hunting_effort <- c(30, 12, 30, 10, 18, 21)
@@ -16,7 +16,7 @@ describe("Get tidy structure from trapping and hunting data", {
   })
   it("Get session", {
     obtained_session <- x$session
-    expected_session <- c(3, 3, 4, 4, 4, 5)
+    expected_session <- c(3, 4, 4, 4, 4, 5)
     expect_equal(obtained_session, expected_session)
   })
 })
