01396b891e795d9f48aef5ed50403a389f8f964c
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-28 11:31:53 -0700

    🛑🧪 Set failing test

--- tests/testthat/test-trapping_hunting.R
+++ tests/testthat/test-trapping_hunting.R
@@ -9,22 +9,28 @@ describe("Get tidy structure from trapping and hunting data", {
     select_columns()
   it("Get hunting effort", {
     obtained_hunting_effort <- x$hunting_effort
-    expected_hunting_effort <- c(30, 12, 30, 10, 18, 21)
+    expected_hunting_effort <- c(30, 12, 6, 30, 9, 10, 18, 21)
     expect_equal(obtained_hunting_effort, expected_hunting_effort)
   })
   it("Get ocassion", {
     obtained_ocassion <- x$ocassion
-    expected_ocassion <- c(12, 14, 14, 15, 16, 20)
+    expected_ocassion <- c(12, 14, 14, 14, 15, 15, 16, 20)
     expect_equal(obtained_ocassion, expected_ocassion)
   })
   it("Get session", {
     obtained_session <- x$session
-    expected_session <- c(3, 4, 4, 4, 4, 5)
+    expected_session <- c(3, 4, 4, 4, 4, 4, 4, 5)
     expect_equal(obtained_session, expected_session)
   })
   it("Get needed columns", {
     obtained_columns <- colnames(x)
     expected_columns <- c("Date", "Grid", "Days_on_terrain", "Hunted_Coati", "Night-traps", "Captured_Coati", "Observed_Coati", "hunting_effort", "ocassion", "session")
     expect_equal(obtained_columns, expected_columns)
+  })
+  it("Group by grid and ocassion", {
+    grouped_data <- group_by_grid_and_ocassion(x)
+    obtained_hunting_effort_grouped_data <- grouped_data$e
+    expected_hunting_effort_grouped_data <- c(21, 10, 18, 18, 30, 30, 9)
+    expect_equal(obtained_hunting_effort_grouped_data, expected_hunting_effort_grouped_data)
   })
 })
