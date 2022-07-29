7da23b2015f8fb60e2a832cfd900d7dc0a45f904
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-29 11:58:58 -0700

    ♻️  Parameterize function

--- R/trapping_hunting.R
+++ R/trapping_hunting.R
@@ -1,5 +1,37 @@
 library(tidyverse)
 
+tidy_from_path_hunting <- function(path) {
+  tidy_from_path_by_method(path, get_removal_and_effort_hunting)
+}
+tidy_from_path_trapping <- function(path) {
+  tidy_from_path_by_method(path, get_removal_and_effort_trapping)
+}
+tidy_from_path_by_method <- function(path, get_removal_and_effort_method) {
+  raw_data <- read_csv(path)
+  trapping_hunting_data <- get_hunting_effort(raw_data) %>%
+    get_ocassion() %>%
+    get_session() %>%
+    select_columns()
+  tidy_trapping <- get_removal_and_effort_method(trapping_hunting_data)
+  return(tidy_trapping)
+}
+
+
+get_removal_and_effort_hunting <- function(data) {
+  get_removal_and_effort_by_method(data, `Hunted_Coati`, `hunting_effort`, "Hunting")
+}
+get_removal_and_effort_trapping <- function(data) {
+  get_removal_and_effort_by_method(data, `Captured_Coati`, `Night-traps`, "Trapping")
+}
+get_removal_and_effort_by_method <- function(data, removed, effort, method) {
+  grouped_data <- data %>%
+    group_by_grid_session_and_ocassion() %>%
+    summarize(r = sum({{ removed }}), e = sum({{ effort }})) %>%
+    ungroup() %>%
+    mutate(Method = method)
+  return(grouped_data)
+}
+
 get_hunting_effort <- function(raw_trapping_hunting) {
   hunting_effort <- raw_trapping_hunting %>%
     mutate(hunting_effort = Days_on_terrain * Persons_on_terrain)
@@ -23,44 +55,7 @@ select_columns <- function(data) {
   return(clean_data)
 }
 
-get_removal_and_effort_hunting <- function(data) {
-  get_removal_and_effort_by_method(data, `Hunted_Coati`, `hunting_effort`, "Hunting")
-}
-
-get_removal_and_effort_trapping <- function(data) {
-  get_removal_and_effort_by_method(data, `Captured_Coati`, `Night-traps`, "Trapping")
-}
-
-get_removal_and_effort_by_method <- function(data, removed, effort, method) {
-  grouped_data <- data %>%
-    group_by_grid_session_and_ocassion() %>%
-    summarize(r = sum({{ removed }}), e = sum({{ effort }})) %>%
-    ungroup() %>%
-    mutate(Method = method)
-  return(grouped_data)
-}
-
 group_by_grid_session_and_ocassion <- function(data) {
   grouped_data <- data %>% group_by(Grid, session, ocassion)
   return(grouped_data)
-}
-
-tidy_from_path_trapping <- function(path){
-  raw_data <- read_csv(path)
-  trapping_hunting_data <- get_hunting_effort(raw_data) %>%
-      get_ocassion() %>%
-      get_session() %>%
-      select_columns()
-  tidy_trapping <- get_removal_and_effort_trapping(trapping_hunting_data)
-  return(tidy_trapping)
-}
-
-tidy_from_path_hunting <- function(path){
-  raw_data <- read_csv(path)
-  trapping_hunting_data <- get_hunting_effort(raw_data) %>%
-      get_ocassion() %>%
-      get_session() %>%
-      select_columns()
-  tidy_hunting <- get_removal_and_effort_hunting(trapping_hunting_data)
-  return(tidy_hunting)
 }
