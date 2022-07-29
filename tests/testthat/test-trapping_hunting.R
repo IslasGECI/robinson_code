e5a613a496eb9ff817c91dc0976f8a2dd83ccf8a
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-29 11:31:09 -0700

    ♻️ Extract function

--- tests/testthat/test-trapping_hunting.R
+++ tests/testthat/test-trapping_hunting.R
@@ -27,45 +27,36 @@ describe("Get tidy structure from trapping and hunting data", {
     expected_columns <- c("Date", "Grid", "Days_on_terrain", "Hunted_Coati", "Night-traps", "Captured_Coati", "Observed_Coati", "hunting_effort", "ocassion", "session")
     expect_equal(obtained_columns, expected_columns)
   })
+  expect_equal_column <- function(grouped_data, column_name, expected_column){
+    obtained_column <- grouped_data[[column_name]]
+    expect_equal(obtained_column, expected_column)
+  }
   it("Group by grid and ocassion", {
     grouped_data <- get_removal_and_effort_trapping(trapping_hunting_data)
-    obtained_trapping_removed_grouped_data <- grouped_data$r
     expected_trapping_removed_grouped_data <- c(0, 0, 2, 2, 0, 0, 0)
-    expect_equal(obtained_trapping_removed_grouped_data, expected_trapping_removed_grouped_data)
+    expect_equal_column(grouped_data, "r", expected_trapping_removed_grouped_data)
 
-    obtained_trapping_effort_grouped_data <- grouped_data$e
     expected_trapping_effort_grouped_data <- c(5, 0, 1, 5, 8, 8, 4)
-    expect_equal(obtained_trapping_effort_grouped_data, expected_trapping_effort_grouped_data)
+    expect_equal_column(grouped_data, "e", expected_trapping_effort_grouped_data)
 
     grouped_data <- get_removal_and_effort_hunting(trapping_hunting_data)
-    obtained_hunting_effort_grouped_data <- grouped_data$e
     expected_hunting_effort_grouped_data <- c(21, 10, 18, 18, 30, 30, 9)
-    expect_equal(obtained_hunting_effort_grouped_data, expected_hunting_effort_grouped_data)
+    expect_equal_column(grouped_data, "e", expected_hunting_effort_grouped_data)
 
-    obtained_hunting_method_grouped_data <- grouped_data$Method
     expected_hunting_method_grouped_data <- rep("Hunting", 7)
-    expect_equal(obtained_hunting_method_grouped_data, expected_hunting_method_grouped_data)
+    expect_equal_column(grouped_data, "Method", expected_hunting_method_grouped_data)
   })
-  it("Test tidy structure for trapping", {
-    tidy_file <- "../data/tidy_trapping.csv"
-    expected_tidy <- read_csv(tidy_file)
-    if (file.exists(tidy_file)){
-      file.remove(tidy_file)
-    }
-    path <- "../data/input_trapping_hunting.csv"
-    obtained_tidy <- tidy_from_path_trapping(path)
-    write_csv(expected_tidy, tidy_file)
+  expect_equal_tidy_structure <- function(tidy_file_path, tidy_from_path_method){
+    expected_tidy <- read_csv(tidy_file_path)
+    obtained_tidy <- tidy_from_path_method(path)
     expect_equal(obtained_tidy, expected_tidy)
+  }
+  it("Test tidy structure for trapping", {
+    tidy_file_path <- "../data/tidy_trapping.csv"
+    expect_equal_tidy_structure(tidy_file_path, tidy_from_path_trapping)
   })
   it("Test tidy structure for hunting", {
-    tidy_file <- "../data/tidy_hunting.csv"
-    expected_tidy <- read_csv(tidy_file)
-    if (file.exists(tidy_file)){
-      file.remove(tidy_file)
-    }
-    path <- "../data/input_trapping_hunting.csv"
-    obtained_tidy <- tidy_from_path_hunting(path)
-    write_csv(expected_tidy, tidy_file)
-    expect_equal(obtained_tidy, expected_tidy)
+    tidy_file_path <- "../data/tidy_hunting.csv"
+    expect_equal_tidy_structure(tidy_file_path, tidy_from_path_hunting)
   })
 })
