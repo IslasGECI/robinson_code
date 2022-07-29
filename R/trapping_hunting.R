5d2853bacf6c1363974e34ace4993814bb6fe688
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-29 12:57:24 -0700

    ✅Pass test

--- R/trapping_hunting.R
+++ R/trapping_hunting.R
@@ -1,22 +1,25 @@
 library(tidyverse)
 
-tidy_from_path_hunting <- function(path) {
+tidy_from_path_hunting <- function(path){
   tidy_from_path_by_method(path, get_removal_and_effort_hunting)
 }
-tidy_from_path_trapping <- function(path) {
+tidy_from_path_trapping <- function(path){
   tidy_from_path_by_method(path, get_removal_and_effort_trapping)
 }
-tidy_from_path_by_method <- function(path, get_removal_and_effort_method) {
+tidy_from_path_by_method <- function(path, get_removal_and_effort_method){
   raw_data <- read_csv(path)
   trapping_hunting_data <- get_hunting_effort(raw_data) %>%
-    get_ocassion() %>%
-    get_session() %>%
-    select_columns()
+      get_ocassion() %>%
+      get_session() %>%
+      select_columns()
   tidy_trapping <- get_removal_and_effort_method(trapping_hunting_data)
   return(tidy_trapping)
 }
 
 
+get_detection_and_effort_observation <- function(data) {
+  get_removal_and_effort_by_method(data, `Observed_Coati`, `hunting_effort`, "Observation")
+}
 get_removal_and_effort_hunting <- function(data) {
   get_removal_and_effort_by_method(data, `Hunted_Coati`, `hunting_effort`, "Hunting")
 }
@@ -50,9 +53,6 @@ get_session <- function(data_trapping_hunting) {
   return(sessions)
 }
 
-get_observations <- function() {
-
-}
 select_columns <- function(data) {
   clean_data <- data %>% select(-c(Sector, Persons_on_terrain))
   return(clean_data)
@@ -62,7 +62,7 @@ group_by_grid_session_and_ocassion <- function(data) {
   grouped_data <- data %>% group_by(Grid, session, ocassion)
   return(grouped_data)
 }
-tidy_from_path_field <- function(path) {
+tidy_from_path_field <- function(path){
   tidy_trapping <- tidy_from_path_trapping(path)
   tidy_hunting <- tidy_from_path_hunting(path)
   return(full_join(tidy_hunting, tidy_trapping))
