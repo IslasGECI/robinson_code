4bd33a149f47ce0db492accde624b9b77a24a242
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-08-01 14:45:40 -0700

    🎨 Make format

--- tests/testthat/test_change_from_tidy_2_data_structures.R
+++ tests/testthat/test_change_from_tidy_2_data_structures.R
@@ -7,8 +7,8 @@ describe("We define the expected data structures", {
   paths <- list("cameras" = path_cameras, "field" = path_field, "coordinates" = path_coordinates)
   tidy_table <- get_tidy_from_field_and_cameras(paths)
   it("Expected Camera-Traps data structure", {
-    method <- "Camera-Traps"  
-    obtained_final_structure <- tidy_2_final(tidy_table %>% filter(Method==method)) 
+    method <- "Camera-Traps"
+    obtained_final_structure <- tidy_2_final(tidy_table %>% filter(Method == method))
     expected_final_structure <- tibble(
       Grid = c(38, 15, 31),
       session = c(4, 4, 4),
@@ -21,13 +21,13 @@ describe("We define the expected data structures", {
     expect_equal(obtained_final_structure, expected_final_structure)
   })
   it("Expected Hunting data structure", {
-    method <- "Hunting"  
-    obtained_final_structure <- tidy_2_final(tidy_table %>% 
-    arrange(ocassion) %>% 
-    filter(Method==method, session==4)) %>%
-    arrange(Grid)
-    
-    path_expected <- "../data/hunting_final_structure.csv" 
+    method <- "Hunting"
+    obtained_final_structure <- tidy_2_final(tidy_table %>%
+      arrange(ocassion) %>%
+      filter(Method == method, session == 4)) %>%
+      arrange(Grid)
+
+    path_expected <- "../data/hunting_final_structure.csv"
     expected_final_structure <- read_csv(path_expected)
     expect_equal(obtained_final_structure, expected_final_structure)
   })
