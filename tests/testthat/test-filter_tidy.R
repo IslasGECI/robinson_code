cc943295f740b185748bd598ebbfd1b36ec70dcd
Mario Villasante <mario.villasante@islas.org.mx>
2022-08-02 14:47:20 -0700

    🛑🧪 Add failing test

--- tests/testthat/test-filter_tidy.R
+++ tests/testthat/test-filter_tidy.R
@@ -6,11 +6,30 @@ describe("Test tidy filters", {
   path_coordinates <- "../data/camera_traps_coordinates.csv"
   paths <- list("cameras" = path_cameras, "field" = path_field, "coordinates" = path_coordinates)
   tidy_table <- get_tidy_from_field_and_cameras(paths)
+  final_structure <- Filter_tidy$new(tidy_table)
   it("Session returns same number", {
-    final_structure <- Filter_tidy$new(tidy_table)
+    obtained_table <- final_structure$select_session(3)
+    obtained_session <- obtained_table$session
+    expected_session <- rep(3,3)
+    expect_equal(obtained_session, expected_session)
     obtained_table <- final_structure$select_session(4)
     obtained_session <- obtained_table$session
-    expected_session <- rep(4,3)
+    expected_session <- rep(4,18)
     expect_equal(obtained_session, expected_session)
+  })
+  it("Filter by method", {
+    method <- "Camera-Traps"
+    obtained_table <- final_structure$select_method(method)
+    obtained_method <- obtained_table$Method
+    expected_method <- rep("Camera-Traps", 3)
+    expect_equal(obtained_method, expected_method)
+  })
+  it("Filter both",{
+    method <- "Hunting" 
+    session <- 4
+    filter_tidy <- Filter_tidy$new(tidy_table)
+    filter_tidy$select_session(session)
+    filtered <- filter_tidy$select_method(method)
+    expect_equal(filtered$Method, rep("Hunting", 5))
   })
 })
