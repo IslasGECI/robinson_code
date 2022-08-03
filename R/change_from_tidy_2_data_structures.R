1a5b25ad2b93325dc17bd1db21931f79615e587d
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-08-03 10:51:46 -0700

    ✅ Pass test

--- R/change_from_tidy_2_data_structures.R
+++ R/change_from_tidy_2_data_structures.R
@@ -1,8 +1,14 @@
 library(tidyverse)
+library(lubridate)
 
-
-get_first_last_week_from_month <- function(month){
-  return(c(13,18))
+get_first_last_week_from_month <- function(month) {
+  first_day_of_month_string <- paste0("2022-", month, "-01")
+  first_day_of_month <- ymd(first_day_of_month_string)
+  first_week_of_month <- week(first_day_of_month)
+  last_of_january <- ymd("2022-01-31")
+  last_day_of_month <- last_of_january %m+% months(month - 1)
+  last_week_of_month <- week(last_day_of_month)
+  return(c(first_week_of_month, last_week_of_month))
 }
 
 #' @export
