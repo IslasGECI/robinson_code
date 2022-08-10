bb190590840340e7b18203db4187b0fc20abe588
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-08-10 16:04:08 -0700

    🎨 Fix format

--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -46,7 +46,7 @@ describe("Define filtered data structure", {
       "2022-04-02 07:25:32",
       "2022-04-04 02:02:42",
       "2022-04-04 02:02:45"
-    ), tz="UTC")
+    ), tz = "UTC")
     expected_ocassion <- c(rep(13, 4), 14, 14)
     expected_id <- c(1, 1, 1, 1, 22, 10)
     expected_coati_count <- c(0, 0, 0, 1, 0, 1)
