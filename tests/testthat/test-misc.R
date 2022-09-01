0e1ca90083f71c89f5613210303210a7b067e369
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-01 11:01:12 -0700

    🛑🧪 Add failing example

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -1,5 +1,12 @@
-test_that("multiplication works", {
+describe("return path of output file", {
+  it("writes april", {
   expected_path <- "data/april_camera_traps.csv"
   obtained_path <- path_for_cameras(month_number = 4)
   expect_equal(obtained_path, expected_path)
+  })
+  it("writes march", {
+  expected_path <- "data/march_camera_traps.csv"
+  obtained_path <- path_for_cameras(month_number = 3)
+  expect_equal(obtained_path, expected_path)
+  })
 })
