da350b9796deb30aa8b0b801443a94597ae39431
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-01 11:21:57 -0700

    🚧 Become public to 'path_for_cameras()'

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 
+#' @export
 path_for_cameras <- function(month_number){
   month_name <- tolower(month.name[month_number])
   base <- glue::glue("data/{month_name}_camera_traps.csv")
