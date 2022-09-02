bee805eecdcbf3a3d7a9158f9c29fc1344ec57b1
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-09-02 10:13:56 -0700

    🎨 Make format

--- R/change_from_raw_2_tidy.R
+++ R/change_from_raw_2_tidy.R
@@ -39,7 +39,7 @@ select_date_ocassion_camera_and_detection_columns <- function(data) {
   )
   return(result)
 }
-get_id_camera_from_relative_path <- function(RelativePath_column){
+get_id_camera_from_relative_path <- function(RelativePath_column) {
   camera_IDs <- as.numeric(gsub(".*?([0-9]+).*", "\\1", RelativePath_column))
   return(camera_IDs)
 }
