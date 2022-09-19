5c37c47841069788d4728e1bbac5d297a1adef93
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-09-19 15:54:47 -0700

    📑🚚 Add observation path

--- tests/testthat/test-observations_2_tidy.R
+++ tests/testthat/test-observations_2_tidy.R
@@ -5,15 +5,15 @@ describe("Get removals and effort from observations", {
   hunting_path <- "../data/hunting_database_for_tests.csv"
   trapping_path <- "../data/trapping_database_for_tests.csv"
   it("test concatenate observations from hunting and trapping", {
-    path_list <- list("hunting" = hunting_path, "trapping" = trapping_path)
+    path_list <- list("hunting" = hunting_path, "trapping" = trapping_path,  "sighting" = observations_path)
     obtained_observations_tidy <- concatenate_observations_from_trapping_and_hunting(path_list)
     expected_observations_tidy <- read_csv("../data/output_concatenate_observations_from_trapping_and_hunting.csv")
     expect_equal(obtained_observations_tidy, expected_observations_tidy)
   })
   it("test tidy observations", {
-    path_list <- list("hunting" = hunting_path, "trapping" = trapping_path, "observations" = observations_path)
-    obtained_trapping_tidy <- tidy_from_path_by_method(trapping_path, get_removal_and_effort_trapping)
-    expected_trapping_tidy <- read_csv("../data/test_tidy_for_trapping.csv")
-    expect_equal(obtained_trapping_tidy, expected_trapping_tidy)
+    path_list <- list("hunting" = hunting_path, "trapping" = trapping_path, "sighting" = observations_path)
+    obtained_observations_tidy <- tidy_from_path_observation(path_list)
+    expected_observations_tidy <- read_csv("../data/test_tidy_for_observations.csv")
+    expect_equal(obtained_observations_tidy, expected_observations_tidy)
   })
 })
