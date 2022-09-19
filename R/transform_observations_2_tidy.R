a5b8493b28f931be3d83cc960b41604071b13df7
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-09-19 15:52:36 -0700

    📑🚚 Add observation path

--- R/transform_observations_2_tidy.R
+++ R/transform_observations_2_tidy.R
@@ -3,9 +3,10 @@ library(tidyverse)
 concatenate_observations_from_trapping_and_hunting <- function(path_list){
     hunting_raw <- read_csv(path_list[["hunting"]])
     trapping_raw <- read_csv(path_list[["trapping"]])
+    observations_raw <- read_csv(path_list[["sighting"]])
     hunting <- subset(hunting_raw, select = -c(Hunted_Coati))
     trapping <- subset(trapping_raw, select = -c(`Night-traps`,Captured_Coati))
-    observations <- rbind(hunting,trapping)
+    observations <- rbind(hunting,trapping,observations_raw)
     return(observations)
 
 }
