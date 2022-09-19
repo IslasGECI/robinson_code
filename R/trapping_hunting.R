96f025a1531fe12e4f68933dd3d8bd7a09e27ac0
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-09-19 15:53:33 -0700

    ✨Implementation of concatenated observations

--- R/trapping_hunting.R
+++ R/trapping_hunting.R
@@ -1,7 +1,10 @@
 library(tidyverse)
 
-tidy_from_path_observation <- function(path) {
-  tidy_from_path_by_method(path, get_detection_and_effort_observation)
+tidy_from_path_observation <- function(path_list) {
+  workdir_path <- getwd()
+  sighting_path <- paste0(workdir_path,"/sighting.csv")
+  write_csv(concatenate_observations_from_trapping_and_hunting(path_list),sighting_path)
+  tidy_from_path_by_method(sighting_path, get_detection_and_effort_observation)
 }
 tidy_from_path_hunting <- function(path) {
   tidy_from_path_by_method(path, get_removal_and_effort_hunting)
@@ -32,7 +35,7 @@ get_removal_and_effort_trapping <- function(data) {
 get_removal_and_effort_by_method <- function(data, removed, effort, method) {
   grouped_data <- data %>%
     group_by_grid_session_and_ocassion() %>%
-    summarize(r = sum({{ removed }}), e = sum({{ effort }})) %>%
+    summarize(r = sum({{ removed }}, na.rm=TRUE), e = sum({{ effort }}, na.rm=TRUE)) %>%
     ungroup() %>%
     mutate(Method = method)
   return(grouped_data)
