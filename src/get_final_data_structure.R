b09ac6d66da2707bb512285b3deeb9497b6c90d4
Mario Villasante <mario.villasante@islas.org.mx>
2022-09-20 16:02:28 -0700

    ♻️  Pack code into loop

--- src/get_final_data_structure.R
+++ src/get_final_data_structure.R
@@ -9,27 +9,17 @@ path_hunting <- "data/raw/robinson_coati_detection_camera_traps/hunting.csv"
 path_list <- list("hunting" = path_hunting, "trapping" = path_trapping, "sighting" = path_sighting)
 path_config <- list("field" = path_list, "cameras" = path_cameras, "coordinates" = path_coordinates)
 tidy_table <- get_tidy_from_field_and_cameras(path_config)
-session <- "2022-4"
-filter_tidy <-Filter_tidy$new(tidy_table)
-filter_tidy$select_session(session)
-filter_tidy$select_method("Hunting")
-hunting_structure <- filter_tidy$spatial()
-write_csv(hunting_structure, file = "data/april_hunting.csv", na = "0")
-
-filter_tidy <-Filter_tidy$new(tidy_table)
-filter_tidy$select_session(session)
-filter_tidy$select_method("Trapping")
-trapping_structure <- filter_tidy$spatial()
-write_csv(trapping_structure, file = "data/april_trapping.csv", na = "0")
-
-filter_tidy <-Filter_tidy$new(tidy_table)
-filter_tidy$select_session(session)
-filter_tidy$select_method("Observation")
-observations_structure <- filter_tidy$spatial()
-write_csv(observations_structure, file = "data/april_observations.csv", na = "0")
-
-filter_tidy <-Filter_tidy$new(tidy_table)
-filter_tidy$select_session(session)
-filter_tidy$select_method("Camera-Traps")
-camera_traps_structure <- filter_tidy$spatial()
-write_csv(camera_traps_structure, file = "data/april_camera_traps.csv", na = "0")
+year <- "2022-"
+methods <- c("Hunting", "Trapping", "Observation", "Camera-Traps")
+for (i in 1:5){
+  for (method in methods){
+    session <- paste0(year,i) 
+    filter_tidy <-Filter_tidy$new(tidy_table)
+    print(session)
+    filter_tidy$select_session(session)
+    filter_tidy$select_method(method)
+    final_structure <- filter_tidy$spatial()
+    output_path <- glue::glue("data/{session}_{method}.csv")
+    write_csv(final_structure, file = output_path, na = "0")
+  }
+}
