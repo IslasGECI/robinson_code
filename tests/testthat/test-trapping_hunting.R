08797498f68ddf17bd574d9abeb252acb412a882
Mario Villasante <mario.villasante@islas.org.mx>
2022-09-20 12:58:22 -0700

    💫🗃️  Update data for tests

--- tests/testthat/test-trapping_hunting.R
+++ tests/testthat/test-trapping_hunting.R
@@ -5,69 +5,48 @@ describe("Get tidy table from trapping and hunting data", {
   hunting_path <- "../data/hunting_database_for_tests.csv"
   trapping_path <- "../data/trapping_database_for_tests.csv"
   path_list <- list("hunting" = hunting_path, "trapping" = trapping_path, "sighting" = sigthing_path)
-  path <- "../data/input_trapping_hunting.csv"
-  raw_data <- read_csv(path, show_col_types = FALSE)
-  trapping_hunting_data <- get_person_day_effort(raw_data) %>%
-    get_ocassion() %>%
-    get_session() %>%
-    select_columns()
-  it("Get hunting effort", {
-    obtained_hunting_effort <- trapping_hunting_data$Person_day_effort
-    expected_hunting_effort <- c(30, 12, 6, 30, 9, 10, 18, 21)
-    expect_equal(obtained_hunting_effort, expected_hunting_effort)
-  })
-  it("Get ocassion", {
-    obtained_ocassion <- trapping_hunting_data$Ocassion
-    expected_ocassion <- c(12, 14, 14, 14, 15, 15, 16, 20)
-    expect_equal(obtained_ocassion, expected_ocassion)
-  })
-  it("Get session", {
-    obtained_session <- trapping_hunting_data$Session
-    expected_session <- c("2022-3", "2022-4", "2022-4", "2022-4", "2022-4", "2022-4", "2022-4", "2022-5")
-    expect_equal(obtained_session, expected_session)
-  })
-  it("Get needed columns", {
-    obtained_columns <- colnames(trapping_hunting_data)
-    expected_columns <- c("Date", "Grid", "Days_on_terrain", "Hunted_Coati", "Night-traps", "Captured_Coati", "Observed_Coati", "Person_day_effort", "Ocassion", "Session")
-    expect_equal(obtained_columns, expected_columns)
-  })
   expect_equal_column <- function(grouped_data, column_name, expected_column) {
     obtained_column <- grouped_data[[column_name]]
     expect_equal(obtained_column, expected_column)
   }
-  it("Group by grid and ocassion", {
-    grouped_data <- get_removal_and_effort_trapping(trapping_hunting_data)
-    expected_trapping_removed_grouped_data <- c(0, 0, 2, 2, 0, 0, 0)
+  it("test get_removal_and_effort_trapping", {
+    trapping_data <- read_csv("../data/input_get_removal_and_effort_trapping.csv", show_col_types = FALSE)
+    grouped_data <- get_removal_and_effort_trapping(trapping_data)
+    expected_trapping_removed_grouped_data <- c(0, 0, 1, 0, 2, 0, 0)
     expect_equal_column(grouped_data, "r", expected_trapping_removed_grouped_data)
-    expected_trapping_effort_grouped_data <- c(5, 0, 1, 5, 8, 8, 4)
+    expected_trapping_effort_grouped_data <- c(5, 1, 2, 4, 5, 8, 8)
     expect_equal_column(grouped_data, "e", expected_trapping_effort_grouped_data)
-
-    grouped_data <- get_removal_and_effort_hunting(trapping_hunting_data)
-    expected_hunting_effort_grouped_data <- c(21, 10, 18, 18, 30, 30, 9)
+  })
+  it("test get_removal_and_effort_hunting",{
+    hunting_data <- read_csv("../data/input_get_removal_and_effort_hunting.csv", show_col_types = FALSE)
+    grouped_data <- get_removal_and_effort_hunting(hunting_data)
+    expected_hunting_effort_grouped_data <- c(21, 12, 2, 2, 5, 36, 2)
     expect_equal_column(grouped_data, "e", expected_hunting_effort_grouped_data)
     expected_hunting_method_grouped_data <- rep("Hunting", 7)
     expect_equal_column(grouped_data, "Method", expected_hunting_method_grouped_data)
-
-    grouped_data <- get_detection_and_effort_observation(trapping_hunting_data)
-    expected_observation_effort_grouped_data <- c(21, 10, 18, 18, 30, 30, 9)
+  })
+  it("test get_removal_and_effort_observation",{
+    sighting_data <- read_csv("../data/input_get_removal_and_effort_sighting.csv", show_col_types = FALSE)
+    grouped_data <- get_detection_and_effort_observation(sighting_data)
+    expected_observation_effort_grouped_data <- c(2, 2, 2, 2, 4, 2, 2, 2, 2)
     expect_equal_column(grouped_data, "e", expected_observation_effort_grouped_data)
-    expected_observation_detection_grouped_data <- c(0, 0, 3, 0, 0, 0, 0)
+    expected_observation_detection_grouped_data <- c(1, 1, 10, 5, 33, 1, 5, 2, 8)
     expect_equal_column(grouped_data, "r", expected_observation_detection_grouped_data)
-    expected_observation_method_grouped_data <- rep("Observation", 7)
+    expected_observation_method_grouped_data <- rep("Observation", 9)
     expect_equal_column(grouped_data, "Method", expected_observation_method_grouped_data)
   })
-  expect_equal_tidy_table <- function(tidy_file_path, tidy_from_path_method) {
+  expect_equal_tidy_table <- function(raw_method_path, tidy_file_path, tidy_from_path_method) {
     expected_tidy <- read_csv(tidy_file_path, show_col_types = FALSE)
-    obtained_tidy <- tidy_from_path_method(path)
+    obtained_tidy <- tidy_from_path_method(raw_method_path)
     expect_equal(obtained_tidy, expected_tidy)
   }
   it("Test tidy table for trapping", {
-    tidy_file_path <- "../data/tidy_trapping.csv"
-    expect_equal_tidy_table(tidy_file_path, tidy_from_path_trapping)
+    tidy_file_path <- "../data/test_tidy_for_trapping.csv"
+    expect_equal_tidy_table(trapping_path, tidy_file_path, tidy_from_path_trapping)
   })
   it("Test tidy table for hunting", {
-    tidy_file_path <- "../data/tidy_hunting.csv"
-    expect_equal_tidy_table(tidy_file_path, tidy_from_path_hunting)
+    tidy_file_path <- "../data/test_tidy_for_hunting.csv"
+    expect_equal_tidy_table(hunting_path, tidy_file_path, tidy_from_path_hunting)
   })
   it("Concatenate tidy tables", {
     tidy_field_path <- "../data/tidy_from_field.csv"
--- tests/testthat/test_raw_2_tidy.R
+++ tests/testthat/test_raw_2_tidy.R
@@ -138,10 +138,13 @@ describe("Get camera traps tidy table", {
     tidy_path_camera_traps <- "../data/tidy_camera_traps.csv"
     expected_tidy_camera_traps <- read_csv(tidy_path_camera_traps, show_col_types = FALSE)
     path_cameras <- "../data/raw_cameras_with_detection.csv"
-    path_field <- "../data/input_trapping_hunting.csv"
+    sigthing_path <- "../data/observations_database_for_tests.csv"
+    hunting_path <- "../data/hunting_database_for_tests.csv"
+    trapping_path <- "../data/trapping_database_for_tests.csv"
     path_coordinates <- "../data/camera_traps_coordinates.csv"
-    paths <- list("cameras" = path_cameras, "field" = path_field, "coordinates" = path_coordinates)
-    obtained_tidy_camera_traps <- tidy_from_path_camera(paths)
+    path_list <- list("hunting" = hunting_path, "trapping" = trapping_path, "sighting" = sigthing_path)
+    path_config <- list("cameras" = path_cameras, "field" = path_list, "coordinates" = path_coordinates)
+    obtained_tidy_camera_traps <- tidy_from_path_camera(path_config)
     expect_equal(obtained_tidy_camera_traps, expected_tidy_camera_traps)
   })
 })
