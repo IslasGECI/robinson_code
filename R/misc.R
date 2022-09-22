23b469d965850ba4d30776a835aaeefd133c72a0
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-09-22 14:52:36 -0700

    🔥⚰️  Delete dead code

--- R/misc.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-
-path_for_cameras <- function(month_number) {
-  month_name <- tolower(month.name[month_number])
-  base <- glue::glue("data/{month_name}_camera_traps.csv")
-  return(base)
-}
