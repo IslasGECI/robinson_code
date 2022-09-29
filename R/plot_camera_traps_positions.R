6100bc79b7bc94b15ea19c2d53ea731cd2234b36
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-09-29 12:48:17 -0700

    ✍🏽 Fix typo

--- R/plot_camera_traps_positions.R
+++ R/plot_camera_traps_positions.R
@@ -3,8 +3,8 @@ library(terra)
 library(ggspatial)
 library(eradicate)
 
-
-plot_camera_positons_in_square_grid <- function(coordinates_path = "data/raw/robinson_coati_detection_camera_traps/camera_trap_coordinates.csv", camera_sightings_path = "data/Camera-Traps.csv", crusoe_shp_path = "data/spatial/Robinson_Coati_Workzones_Simple.shp", square_grid_path = "data/spatial/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp", plot_output_path) {
+#' @export
+plot_camera_positions_in_square_grid <- function(coordinates_path = "data/raw/robinson_coati_detection_camera_traps/camera_trap_coordinates.csv", camera_sightings_path = "data/Camera-Traps.csv", crusoe_shp_path = "data/spatial/Robinson_Coati_Workzones_Simple.shp", square_grid_path = "data/spatial/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp", plot_output_path) {
   crusoe_shp <- sf::read_sf(crusoe_shp_path)
   square_grid <- sf::read_sf(square_grid_path)
   camera_observations <- get_camera_observations(camera_sightings_path = camera_sightings_path, coordinates_path = coordinates_path)
--- tests/testthat/test-plot_camera_traps_positions.R
+++ tests/testthat/test-plot_camera_traps_positions.R
@@ -5,7 +5,7 @@ describe("Get camera traps positions", {
     camera_sightings_path <- "../data/april_camera_traps_2022.csv"
     coordinates_path <- "../data/camera_traps_coordinates_april_2022.csv"
     plot_output_path <- "../data/plot_crusoe.png"
-    plot_camera_positons_in_square_grid(coordinates_path = coordinates_path, camera_sightings_path = camera_sightings_path, crusoe_shp_path = crusoe_shp_path, square_grid_path = square_grid_path, plot_output_path = plot_output_path)
+    plot_camera_positions_in_square_grid(coordinates_path = coordinates_path, camera_sightings_path = camera_sightings_path, crusoe_shp_path = crusoe_shp_path, square_grid_path = square_grid_path, plot_output_path = plot_output_path)
     obtanied_hash <- as.vector(tools::md5sum(plot_output_path))
     expected_hash <- c("042316766181beb35f055208ce4b5f1f")
     expect_equal(obtanied_hash, expected_hash)
