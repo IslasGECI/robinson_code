414fab99f9d07a4cf0fcb48f2a585008f688e98c
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-09-29 09:21:29 -0700

    🩹⬆️ Fix dependencies in install

--- R/plot_camera_traps_positions.R
+++ R/plot_camera_traps_positions.R
@@ -1,13 +1,12 @@
 library(tidyverse)
-library(sf)
 library(terra)
 library(ggspatial)
 library(eradicate)
 
 
 plot_camera_positons_in_square_grid <- function(coordinates_path = "data/raw/robinson_coati_detection_camera_traps/camera_trap_coordinates.csv", camera_sightings_path = "data/Camera-Traps.csv", crusoe_shp_path = "data/spatial/Robinson_Coati_Workzones_Simple.shp", square_grid_path = "data/spatial/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp", plot_output_path) {
-  crusoe_shp <- read_sf(crusoe_shp_path)
-  square_grid <- read_sf(square_grid_path)
+  crusoe_shp <- sf::read_sf(crusoe_shp_path)
+  square_grid <- sf::read_sf(square_grid_path)
   camera_coordinates <- read_csv(coordinates_path, show_col_types = FALSE)
   camera_sightings <- read_csv(camera_sightings_path, show_col_types = FALSE)
   # remove camera coords with ID == NA
@@ -23,12 +22,12 @@ plot_camera_positons_in_square_grid <- function(coordinates_path = "data/raw/rob
   cobs_l <- camera_coordinates %>%
     filter(ID %in% cobs_n$ID) %>%
     select(ID, X = Easting, Y = Norting)
-  cobs_l <- st_as_sf(cobs_l, coords = c("X", "Y"), crs = 32717)
+  cobs_l <- sf::st_as_sf(cobs_l, coords = c("X", "Y"), crs = 32717)
 
   # Quick plot of grid cells and camera locations
   png(file = plot_output_path, width = 600, height = 350)
-  plot(st_geometry(crusoe_shp))
-  plot(st_geometry(square_grid), add = TRUE)
-  plot(st_geometry(cobs_l), add = TRUE, pch = 16, col = "red")
+  plot(sf::st_geometry(crusoe_shp))
+  plot(sf::st_geometry(square_grid), add = TRUE)
+  plot(sf::st_geometry(cobs_l), add = TRUE, pch = 16, col = "red")
   dev.off()
 }
