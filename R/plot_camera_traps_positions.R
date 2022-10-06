8db6c30c354337d4eb8d2fbc12f4242ae2d4f426
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-06 14:25:39 -0700

    ♻️ Change path to object

--- R/plot_camera_traps_positions.R
+++ R/plot_camera_traps_positions.R
@@ -4,8 +4,7 @@ library(ggspatial)
 library(eradicate)
 
 #' @export
-plot_camera_positions_in_square_grid <- function(camera_sightings, crusoe_shp_path = "data/spatial/Robinson_Coati_Workzones_Simple.shp", square_grid_path = "data/spatial/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp", plot_output_path) {
-  crusoe_shp <- sf::read_sf(crusoe_shp_path)
+plot_camera_positions_in_square_grid <- function(camera_sightings, square_grid_path = "data/spatial/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp", plot_output_path, crusoe_shp) {
   square_grid <- sf::read_sf(square_grid_path)
   camera_locations <- camera_sightings[["locations"]]
   # Quick plot of grid cells and camera locations
--- tests/testthat/test-plot_camera_traps_positions.R
+++ tests/testthat/test-plot_camera_traps_positions.R
@@ -2,12 +2,13 @@ describe("Get camera traps positions", {
   camera_locations <- read_csv("../data/input_plot_camera_positions_square_locations.csv", show_col_types = FALSE)
   camera_sightings <- list("locations" = sf::st_as_sf(camera_locations, wkt = "geometry"))
   crusoe_shp_path <- "../data/Robinson_Coati_Workzones_Simple.shp"
+  crusoe_shp <- sf::read_sf(crusoe_shp_path)
   it("Hash test for plot_crusoe", {
     square_grid_path <- "../data/Robinson_Coati_1kmGrid_SubsetCameraGrids.shp"
     camera_sightings_path <- "../data/april_camera_traps_2022.csv"
     coordinates_path <- "../data/camera_traps_coordinates_april_2022.csv"
     plot_output_path <- "../data/plot_crusoe.png"
-    plot_camera_positions_in_square_grid(camera_sightings = camera_sightings, crusoe_shp_path = crusoe_shp_path, square_grid_path = square_grid_path, plot_output_path = plot_output_path)
+    plot_camera_positions_in_square_grid(camera_sightings = camera_sightings, square_grid_path = square_grid_path, plot_output_path = plot_output_path, crusoe_shp)
     obtanied_hash <- as.vector(tools::md5sum(plot_output_path))
     expected_hash <- c("042316766181beb35f055208ce4b5f1f")
     expect_equal(obtanied_hash, expected_hash)
