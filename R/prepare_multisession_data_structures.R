6e43d15bcfda7e7178966a83b69ada5644ed556f
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-20 12:17:48 -0700

    ♻️  Move public to private methods

--- R/prepare_multisession_data_structures.R
+++ R/prepare_multisession_data_structures.R
@@ -4,15 +4,9 @@ Multisession <- R6::R6Class("Multisession",
     initialize = function(all_data) {
       self$all_data <- all_data
     },
-    present_grids = function() {
-      return(unique(self$all_data$Grid))
-    },
-    present_sessions = function() {
-      return(unique(self$all_data$Session))
-    },
     expanded_grid_session = function() {
-      present_grids <- self$present_grids()
-      present_sessions <- self$present_sessions()
+      present_grids <- private$present_grids()
+      present_sessions <- private$present_sessions()
       return(expand.grid(present_grids, present_sessions))
     },
     get_complete_multisession = function() {
@@ -25,20 +19,28 @@ Multisession <- R6::R6Class("Multisession",
       return(expanded_data %>% arrange(lubridate::ym(expanded_data$Session), Grid))
     },
     rename_session = function() {
-      number_of_grids <- length(self$present_grids())
-      number_of_sessions <- length(self$present_sessions())
+      number_of_grids <- length(private$present_grids())
+      number_of_sessions <- length(private$present_sessions())
       sessions <- 1:number_of_sessions
       sorted_data <- self$sort_by_session_and_grid()
       renamed_sessions <- sorted_data %>% mutate(Session = rep(sessions, each = number_of_grids))
       return(renamed_sessions)
     },
     setup_data_for_multisession = function(){
-      number_of_grids <- length(self$present_grids())
-      number_of_sessions <- length(self$present_sessions())
+      number_of_grids <- length(private$present_grids())
+      number_of_sessions <- length(private$present_sessions())
       sessions <- 1:number_of_sessions
       sorted_data <- self$sort_by_session_and_grid()
       renamed_sessions <- sorted_data %>% mutate(Session = rep(sessions, each=number_of_grids))
       return(renamed_sessions)
+    }
+  ),
+  private = list(
+    present_grids = function() {
+      return(unique(self$all_data$Grid))
+    },
+    present_sessions = function() {
+      return(unique(self$all_data$Session))
     }
   )
 )
--- tests/testthat/test-prepare_multisession_data_structures.R
+++ tests/testthat/test-prepare_multisession_data_structures.R
@@ -4,16 +4,6 @@ describe("Prepare multisession data to fit in models", {
   it("Object exists", {
     expect_true(exists("obtained_object"))
   })
-  it("Obtain present grids and sessions", {
-    expected_present_grids <- c(9, 24, 28, 31, 48, 50)
-    obtained_present_grids <- obtained_object$present_grids()
-    is_all_grids <- all(obtained_present_grids %in% expected_present_grids)
-    expect_true(is_all_grids)
-    expected_present_sessions <- c("2021-8", "2021-10", "2022-1", "2022-3", "2022-4", "2022-5")
-    obtained_present_sessions <- obtained_object$present_sessions()
-    is_all_sessions <- all(obtained_present_sessions %in% expected_present_sessions)
-    expect_true(is_all_sessions)
-  })
   it("Obtain expanded grids and sessions", {
     expected_rows_number <- 36
     obtained_rows_number <- nrow(obtained_object$expanded_grid_session())
