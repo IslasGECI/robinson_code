8c3ed4ea33ebf68dde92c3d6628a9deb59331d04
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-20 10:18:56 -0700

    ✅ Pass test

--- R/prepare_multisession_data_structures.R
+++ R/prepare_multisession_data_structures.R
@@ -1,8 +1,14 @@
-Multisession <- R6::R6Class("Multisession", 
+Multisession <- R6::R6Class("Multisession",
   public = list(
-    all_months = NULL,
-    initialize = function() {
-      self$all_months <- NULL
+    all_data = NULL,
+    initialize = function(all_data) {
+      self$all_data <- all_data
+    },
+    unique_grids = function(){
+      return(unique(self$all_data$Grid))
+    },
+    unique_sessions = function(){
+      return(unique(self$all_data$Session))
     }
   )
-  )
+)
--- tests/testthat/test-prepare_multisession_data_structures.R
+++ tests/testthat/test-prepare_multisession_data_structures.R
@@ -1,7 +1,17 @@
 describe("Prepare multisession data to fit in models", {
+  input <- readr::read_csv("data/Hunting.csv")
+  obtained_object <- Multisession$new(input)
   it("Object exists", {
-    input <- read_csv("data/Hunting.csv")
-    obtained_object <- Multisession$new(input)
     expect_true(exists("obtained_object"))
+  })
+  it("Obtain unique grids and sessions",{
+    expected_unique_grids <- c(9,24,28,31,48,50)
+    obtained_unique_grids <- obtained_object$unique_grids()
+    is_all_grids <- all(obtained_unique_grids %in% expected_unique_grids)
+    expect_true(is_all_grids)
+    expected_unique_sessions <- c("2021-8","2021-10","2022-1","2022-3","2022-4","2022-5")
+    obtained_unique_sessions <- obtained_object$unique_sessions()
+    is_all_sessions <- all(obtained_unique_sessions %in% expected_unique_sessions)
+    expect_true(is_all_sessions)
   })
 })
