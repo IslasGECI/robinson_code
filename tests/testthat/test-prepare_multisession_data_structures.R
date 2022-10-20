7a8e6f84045ef06e31ada6f4df155d0e4716613c
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-20 09:51:08 -0700

    💥🧪 Failing test

--- tests/testthat/test-prepare_multisession_data_structures.R
+++ tests/testthat/test-prepare_multisession_data_structures.R
@@ -1,6 +1,7 @@
-describe("Prepare multisession data to fit in models",{
-  it("Object exists",{
-    obtained_object <- Multisession$new()
+describe("Prepare multisession data to fit in models", {
+  it("Object exists", {
+    input <- read_csv("data/Hunting.csv")
+    obtained_object <- Multisession$new(input)
     expect_true(exists("obtained_object"))
   })
 })
