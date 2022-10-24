56af2a19c7307e32eb96149d19dd9d9fc2bdabdf
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-24 15:33:42 -0700

    ♻️  Extract variable

--- tests/testthat/test-get_multisession_predictions.R
+++ tests/testthat/test-get_multisession_predictions.R
@@ -3,13 +3,13 @@ describe("Get camera traps observations", {
   coordinates_path <- "../data/camera_traps_coordinates_april_2022.csv"
   camera_sightings <- read_csv(camera_sightings_path) %>% mutate(session = Session)
   obtained_camera_observations <- get_camera_observations_multisession(camera_sightings = camera_sightings, coordinates_path = coordinates_path)
+  obtained_detections <- obtained_camera_observations[["detections"]]
   it("Test cameras coordinates and grids match", {
     coordinates_path <- "../data/camera_traps_coordinates_april_2022_without_24_and_50.csv"
     camera_sightings <- read_csv(camera_sightings_path) %>% mutate(session = Session)
     expect_error(get_camera_observations_multisession(camera_sightings = camera_sightings, coordinates_path = coordinates_path))
   })
   it("At least one element has wrong dimension number", {
-    obtained_detections <- obtained_camera_observations[["detections"]]
     number_of_session <- 6
     number_of_sites <- 6
     obtained_rows <- nrow(obtained_detections)
@@ -17,7 +17,6 @@ describe("Get camera traps observations", {
     expect_equal(obtained_rows, expected_rows)
   })
   it("Detections have NA's", {
-    obtained_detections <- obtained_camera_observations[["detections"]]
     obtained_NAs <- sum(is.na(obtained_detections))
     expected_NAs <- 209
     expect_equal(obtained_NAs, expected_NAs)
