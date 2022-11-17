d7e4d4029c2e202c8010337d3fce924a4cdd39e9
Mario Villasante <mario.villasante@islas.org.mx>
2022-11-17 14:38:32 -0800

    ✨🍱 Change filter data to ramsey format

--- R/camera_traps_2_ramsey_format.R
+++ R/camera_traps_2_ramsey_format.R
@@ -1,3 +1,7 @@
 camera_traps_2_ramsey_format <- function(camera_sightings) {
-  return(NULL)
+  camera_data <- Filter_Camera_by_Month$new(camera_sightings)
+  filtered_data <- camera_data$filter_data_for_multisession()
+  multisession <- Multisession$new(filtered_data)
+  ramsey_format_data <- multisession$data_for_multisession %>% rename(session = Session)
+  return(ramsey_format_data)
 }
--- tests/testthat/test-camera_traps_2_ramsey_format.R
+++ tests/testthat/test-camera_traps_2_ramsey_format.R
@@ -5,8 +5,9 @@ testthat::describe("Prepare camera traps data to Ramsey format", {
     camera_sightings <- read_csv("../data/input_ramsey_format.csv")
     obtained <- camera_traps_2_ramsey_format(camera_sightings)
     obtained_path <- "../data/ramsey_format.csv"
+    write_csv(obtained, obtained_path)
     obtained_hash <- tools::md5sum(obtained_path)
-    expected_hash <- "426e2eba1b5ca13c8e3e3197bb9841f5"
+    expected_hash <- "cecb6bde91f0e8f99139fec580ccbb14"
     expect_equal(obtained_hash[[1]], expected_hash)
   })
 })
