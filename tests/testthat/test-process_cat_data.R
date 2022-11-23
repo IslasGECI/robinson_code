4cf29fed875364f72f91675595010202c16321e9
Mario Villasante <mario.villasante@islas.org.mx>
2022-11-22 16:12:34 -0800

    🔴 Add test

---
+++ tests/testthat/test-process_cat_data.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+testthat::describe("Define filtered data structure for cat detection", {
+  it("Expected filtered data structure for cats", {
+    path <- "../data/raw_cameras.csv"
+    data <- read_csv(path, show_col_types = FALSE)
+    expected_dates <- as.POSIXct(c(
+      "2022-04-02 07:25:23",
+      "2022-04-02 07:25:26",
+      "2022-04-02 07:25:29",
+      "2022-04-02 07:25:32",
+      "2022-04-04 02:02:42",
+      "2022-04-04 02:02:45"
+    ), tz = "UTC")
+    expected_ocassion <- c(rep(14, 4), 15, 15)
+    expected_id <- c(1, 1, 1, 1, 22, 10)
+    expected_cat_count <- c(0, 0, 1, 0, 1, 1)
+    expected_structure <- tibble(
+      date = c(expected_dates),
+      Ocassion = c(expected_ocassion),
+      camera_id = expected_id,
+      cat_count = expected_cat_count
+    )
+    obtained_structure <- select_date_ocassion_camera_and_detection_columns_for_cat(data)
+    expect_equal(expected_structure, obtained_structure)
+  })
+})
