77de3d95222aa41bdd091db2c8f8f1a1a0d4b787
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-29 14:58:35 -0800

    ✨ Script for recipe

---
+++ src/join_predictions_with_count_of_cells_with_data.R
@@ -0,0 +1,6 @@
+library(robinson)
+library(tidyverse)
+
+path_predictions <- "/workdir/preds_1km_grid-cats.csv"
+multisession_data_path <- "/workdir/data/multisession-Camera-Traps-Cats.csv"
+write_prediction_and_cells_with_camera_data(path_predictions, multisession_data_path)
