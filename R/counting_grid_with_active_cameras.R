18026e6bdc10802fc20f3e00ddc268fe19cdccd5
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-29 12:05:24 -0800

    ♻️  Organize functions

--- R/counting_grid_with_active_cameras.R
+++ R/counting_grid_with_active_cameras.R
@@ -1,37 +1,22 @@
 library(tidyverse)
 
-select_effort <- function(multi_data) {
-  result <- multi_data %>% select(-starts_with("r"))
-  return(result)
+#' @export
+count_cells_with_camera_data_from_multisession_data <- function(multi_data) {
+  multi_data %>%
+    get_effort_by_grid_and_season_in_long_table() %>%
+    drop_grid_without_effort() %>%
+    count_cells_with_camera_data_by_session()
 }
-
-get_long_table_for_effort <- function(selected_effort) {
-  result <- selected_effort %>%
-    pivot_longer(cols = starts_with("e_"), names_to = "week", names_prefix = "e_", values_to = "e")
-  return(result)
-}
-
-effort_by_grid_and_season <- function(long_table) {
-  result <- long_table %>%
-    group_by(session, Grid) %>%
-    summarize(e = sum(e)) %>%
-    ungroup() %>%
-    select(Grid, session, e)
-  return(result)
-}
-
 get_effort_by_grid_and_season_in_long_table <- function(multi_data) {
   multi_data %>%
     select_effort() %>%
     get_long_table_for_effort() %>%
     effort_by_grid_and_season()
 }
-
 drop_grid_without_effort <- function(resumed_effort) {
   resumed_effort %>%
     filter(e != 0)
 }
-
 count_cells_with_camera_data_by_session <- function(grids_with_data) {
   grids_with_data %>%
     group_by(session) %>%
@@ -39,9 +24,20 @@ count_cells_with_camera_data_by_session <- function(grids_with_data) {
     ungroup()
 }
 
-count_cells_with_camera_data_from_multisession_data <- function(multi_data) {
-  multi_data %>%
-    get_effort_by_grid_and_season_in_long_table() %>%
-    drop_grid_without_effort() %>%
-    count_cells_with_camera_data_by_session()
+select_effort <- function(multi_data) {
+  result <- multi_data %>% select(-starts_with("r"))
+  return(result)
+}
+get_long_table_for_effort <- function(selected_effort) {
+  result <- selected_effort %>%
+    pivot_longer(cols = starts_with("e_"), names_to = "week", names_prefix = "e_", values_to = "e")
+  return(result)
+}
+effort_by_grid_and_season <- function(long_table) {
+  result <- long_table %>%
+    group_by(session, Grid) %>%
+    summarize(e = sum(e)) %>%
+    ungroup() %>%
+    select(Grid, session, e)
+  return(result)
 }
