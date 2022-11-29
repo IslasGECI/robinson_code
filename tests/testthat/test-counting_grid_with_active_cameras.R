c6fd8fc04a7bd4a93ad4ca08b8744c5b36ed14d5
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-29 12:05:02 -0800

    🔥 Remove unit tests

--- tests/testthat/test-counting_grid_with_active_cameras.R
+++ tests/testthat/test-counting_grid_with_active_cameras.R
@@ -1,28 +1,15 @@
 library(tidyverse)
+
 testthat::describe("Counting active cameras", {
   multi_data <- read_csv("../data/multisesion_camera_traps_cats.csv", show_col_types = FALSE)
+  it("🪙 count_cells_with_camera_data_from_multisession_data", {
+    obtained <- count_cells_with_camera_data_from_multisession_data(multi_data)
+    expected <- read_csv("../data/filter_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
+    expect_equal(obtained, expected)
+  })
   it("get_effort_by_grid_and_season_in_long_table 🥈", {
     obtained <- get_effort_by_grid_and_season_in_long_table(multi_data)
     expected <- read_csv("../data/count_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
-    expect_equal(obtained, expected)
-  })
-  it("Filter inactive cameras", {
-    resumed_effort <- read_csv("../data/count_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
-    obtained <- drop_grid_without_effort(resumed_effort)
-    obtained_rows <- nrow(obtained)
-    expected_rows <- 5
-    expect_equal(obtained_rows, expected_rows)
-  })
-  it("count_cells_with_camera_data_by_session", {
-    resumed_effort <- read_csv("../data/count_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
-    droped_grid <- drop_grid_without_effort(resumed_effort)
-    obtained <- count_cells_with_camera_data_by_session(droped_grid)
-    expected <- read_csv("../data/filter_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
-    expect_equal(obtained, expected)
-  })
-  it("🪙 ", {
-    obtained <- count_cells_with_camera_data_from_multisession_data(multi_data)
-    expected <- read_csv("../data/filter_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
     expect_equal(obtained, expected)
   })
 })
