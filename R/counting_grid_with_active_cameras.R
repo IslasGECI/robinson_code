602d555aa3a3122e4d6d1e64180587c5eb3506e5
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-29 10:36:16 -0800

    ✅🧪 Pass test

--- R/counting_grid_with_active_cameras.R
+++ R/counting_grid_with_active_cameras.R
@@ -4,3 +4,10 @@ select_effort <- function(multi_data) {
   result <- multi_data %>% select(-starts_with("r"))
   return(result)
 }
+
+get_long_table_for_effort <- function(selected_effort) {
+  result <- selected_effort %>%
+    pivot_longer(cols = starts_with("e_"), names_to = "week", names_prefix = "e_", values_to = "e")
+  print(result)
+  return(result)
+}
--- tests/testthat/test-counting_grid_with_active_cameras.R
+++ tests/testthat/test-counting_grid_with_active_cameras.R
@@ -7,6 +7,17 @@ testthat::describe("Counting active cameras", {
     expected_n_cols <- 8
     expect_equal(obtained_n_cols, expected_n_cols)
   })
+  it("Long table -delete me-", {
+    multi_data <- read_csv("../data/multisesion_camera_traps_cats.csv", show_col_types = FALSE)
+    selected_data <- select_effort(multi_data)
+    obtained <- get_long_table_for_effort(selected_data)
+    obtained_n_rows <- nrow(obtained)
+    expected_n_rows <- 9 * 6
+    expect_equal(obtained_n_rows, expected_n_rows)
+    obtained_n_cols <- ncol(obtained)
+    expected_n_cols <- 4
+    expect_equal(obtained_n_cols, expected_n_cols)
+  })
   it("Filter inactive cameras", {
 
   })
