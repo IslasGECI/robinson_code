6d057234c24d6933da95bd3145be2eb1913afd78
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-29 10:56:47 -0800

    🧪🥈 Test for effort by grid and season

--- R/counting_grid_with_active_cameras.R
+++ R/counting_grid_with_active_cameras.R
@@ -8,6 +8,14 @@ select_effort <- function(multi_data) {
 get_long_table_for_effort <- function(selected_effort) {
   result <- selected_effort %>%
     pivot_longer(cols = starts_with("e_"), names_to = "week", names_prefix = "e_", values_to = "e")
-  print(result)
   return(result)
+}
+
+effort_by_grid_and_season <- function(long_table) {
+    result <- long_table %>%
+      group_by(session, Grid) %>%
+      summarize(e = sum(e)) %>%
+      ungroup() %>%
+      select(Grid, session, e)
+    return(result)
 }
--- tests/testthat/test-counting_grid_with_active_cameras.R
+++ tests/testthat/test-counting_grid_with_active_cameras.R
@@ -1,14 +1,13 @@
 library(tidyverse)
 testthat::describe("Counting active cameras", {
+  multi_data <- read_csv("../data/multisesion_camera_traps_cats.csv", show_col_types = FALSE)
   it("Selecting effort", {
-    multi_data <- read_csv("../data/multisesion_camera_traps_cats.csv", show_col_types = FALSE)
     obtained <- select_effort(multi_data)
     obtained_n_cols <- ncol(obtained)
     expected_n_cols <- 8
     expect_equal(obtained_n_cols, expected_n_cols)
   })
   it("Long table -delete me-", {
-    multi_data <- read_csv("../data/multisesion_camera_traps_cats.csv", show_col_types = FALSE)
     selected_data <- select_effort(multi_data)
     obtained <- get_long_table_for_effort(selected_data)
     obtained_n_rows <- nrow(obtained)
@@ -17,6 +16,17 @@ testthat::describe("Counting active cameras", {
     obtained_n_cols <- ncol(obtained)
     expected_n_cols <- 4
     expect_equal(obtained_n_cols, expected_n_cols)
+  })
+  it("Sum effort -delete me-", {
+    long_table <- multi_data %>% select_effort() %>% get_long_table_for_effort()
+    obtained <- effort_by_grid_and_season(long_table)
+    expected <- read_csv("../data/count_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
+    expect_equal(obtained, expected)
+  })
+  it("get_effort_by_grid_and_season_in_long_table -delete me-", {
+    obtained <- get_effort_by_grid_and_season_in_long_table(multi_data)
+    expected <- read_csv("../data/count_cameras_with_effort_from_multisesion.csv", show_col_types = FALSE)
+    expect_equal(obtained, expected)
   })
   it("Filter inactive cameras", {
 
