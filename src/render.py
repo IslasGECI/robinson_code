ab208fc8414b403153ae9c473a97c3b950bfb4b3
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-09 11:12:54 -0800

    ➕💬 Add summary_path argument

--- src/render.py
+++ src/render.py
@@ -4,11 +4,15 @@ import typer
 app = typer.Typer()
 
 default_report = "cat_population_estimation"
+summary_path = "tests/data/results.json"
 
 
 @app.command()
-def write_dot_tex(report_name: str = typer.Argument(default_report)):
-    jr.write_tex(report_name)
+def write_dot_tex(
+    report_name: str = typer.Argument(default_report),
+    summary_path: str = typer.Argument(summary_path),
+):
+    jr.write_tex(report_name, summary_path)
 
 
 if __name__ == "__main__":
