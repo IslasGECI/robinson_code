db40d94000b9637ff4806c7a602835983495e8d0
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-10 13:27:25 -0800

    ✅🧪 Add method 'write()'

--- R/join_prediction_with_count_of_cells_with_data.R
+++ R/join_prediction_with_count_of_cells_with_data.R
@@ -1,16 +1,21 @@
-
+#' @export
 Writer_Prediction_And_Cells_With_Camera_Data <- R6::R6Class("Writer",
   public = list(
-    predictions_path= NULL,
+    predictions_path = NULL,
     multisession_data_path = NULL,
+    output_path = NULL,
     initialize = function(species) {
-      
       self$predictions_path <- private$input_prediction[[species]]
-      self$multisession_data_path <- private$input_multisession[[species]] 
+      self$multisession_data_path <- private$input_multisession[[species]]
+      self$output_path <- private$csv_path[[species]]
+    },
+    write = function() {
+      write_prediction_and_cells_with_camera_data(self$predictions_path, self$multisession_data_path, self$output_path)
     }
   ),
   private = list(
     input_multisession = list(Coatis = "data/multisession-Camera-Traps.csv", Cats = "data/multisession-Camera-Traps-Cats.csv"),
-    input_prediction = list(Coatis = "data/preds_1km_grid.csv", Cats = "data/preds_1km_grid-cats.csv")
+    input_prediction = list(Coatis = "data/preds_1km_grid.csv", Cats = "data/preds_1km_grid-cats.csv"),
+    csv_path = list(Coatis = "/workdir/prediction_with_count_cells_coatis.csv", Cats = "/workdir/prediction_with_count_cells.csv")
   )
 )
--- tests/testthat/test-join_prediction_with_count_of_cells_with_data.R
+++ tests/testthat/test-join_prediction_with_count_of_cells_with_data.R
@@ -1,12 +1,14 @@
 testthat::describe("The paths are right", {
   it("Cats", {
-  Writer <- Writer_Prediction_And_Cells_With_Camera_Data$new("Cats")
-  expect_equal(Writer$predictions_path, "data/preds_1km_grid-cats.csv")
-  expect_equal(Writer$multisession_data_path, "data/multisession-Camera-Traps-Cats.csv")
-})
+    Writer <- Writer_Prediction_And_Cells_With_Camera_Data$new("Cats")
+    expect_equal(Writer$predictions_path, "data/preds_1km_grid-cats.csv")
+    expect_equal(Writer$multisession_data_path, "data/multisession-Camera-Traps-Cats.csv")
+    expect_equal(Writer$output_path, "/workdir/prediction_with_count_cells.csv")
+  })
   it("Coati", {
-  Writer <- Writer_Prediction_And_Cells_With_Camera_Data$new("Coatis")
-  expect_equal(Writer$predictions_path, "data/preds_1km_grid.csv")
-  expect_equal(Writer$multisession_data_path, "data/multisession-Camera-Traps.csv")
-})
+    Writer <- Writer_Prediction_And_Cells_With_Camera_Data$new("Coatis")
+    expect_equal(Writer$predictions_path, "data/preds_1km_grid.csv")
+    expect_equal(Writer$multisession_data_path, "data/multisession-Camera-Traps.csv")
+    expect_equal(Writer$output_path, "/workdir/prediction_with_count_cells_coatis.csv")
+  })
 })
