c5a9746c45fb76acecaef291b12928d0a1e05e21
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-10 13:44:03 -0800

    ✅🧪 Throw error with incorrect species name

--- R/join_prediction_with_count_of_cells_with_data.R
+++ R/join_prediction_with_count_of_cells_with_data.R
@@ -5,6 +5,7 @@ Writer_Prediction_And_Cells_With_Camera_Data <- R6::R6Class("Writer",
     multisession_data_path = NULL,
     output_path = NULL,
     initialize = function(species) {
+      private$check_species(species)
       self$predictions_path <- private$input_prediction[[species]]
       self$multisession_data_path <- private$input_multisession[[species]]
       self$output_path <- private$csv_path[[species]]
@@ -16,6 +17,10 @@ Writer_Prediction_And_Cells_With_Camera_Data <- R6::R6Class("Writer",
   private = list(
     input_multisession = list(Coatis = "data/multisession-Camera-Traps.csv", Cats = "data/multisession-Camera-Traps-Cats.csv"),
     input_prediction = list(Coatis = "data/preds_1km_grid.csv", Cats = "data/preds_1km_grid-cats.csv"),
-    csv_path = list(Coatis = "/workdir/prediction_with_count_cells_coatis.csv", Cats = "/workdir/prediction_with_count_cells.csv")
+    csv_path = list(Coatis = "/workdir/prediction_with_count_cells_coatis.csv", Cats = "/workdir/prediction_with_count_cells.csv"),
+    check_species = function(species) {
+      robinson_species <- names(private$csv_path)
+      rlang::arg_match0(species, robinson_species)
+    }
   )
 )
--- tests/testthat/test-join_prediction_with_count_of_cells_with_data.R
+++ tests/testthat/test-join_prediction_with_count_of_cells_with_data.R
@@ -11,4 +11,7 @@ testthat::describe("The paths are right", {
     expect_equal(Writer$multisession_data_path, "data/multisession-Camera-Traps.csv")
     expect_equal(Writer$output_path, "/workdir/prediction_with_count_cells_coatis.csv")
   })
+  it("Without species", {
+    expect_error(Writer_Prediction_And_Cells_With_Camera_Data$new())
+  })
 })
