cdea5b8451fb68a32d3f54f157b28e6e0026ffe1
Mario Villasante <mario.villasante@islas.org.mx>
2023-01-10 13:28:11 -0800

    🚀 Use new class

--- src/join_predictions_with_count_of_cells_with_data.R
+++ src/join_predictions_with_count_of_cells_with_data.R
@@ -4,6 +4,5 @@ library(optparse)
 
 opciones <- cli_get_multisession_predictions()
 species <- opciones[["species"]]
-path_predictions <- output_prediction[[species]]
-multisession_data_path <- input_multisession[[species]]
-write_prediction_and_cells_with_camera_data(path_predictions, multisession_data_path)
+Writer <- Writer_Prediction_And_Cells_With_Camera_Data$new(species)
+Writer$write()
