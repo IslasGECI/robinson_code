ded199b65ffdbff26aaf942f1af447a9eac3124c
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-01-11 11:35:58 -0800

    ♻️  Extract function

--- R/summary_for_report.R
+++ R/summary_for_report.R
@@ -4,16 +4,20 @@ get_prediction <- function(predictions_df, month) {
 }
 
 get_max <- function(predictions_df, ignore_month = NULL) {
-  selected_data <- predictions_df %>% filter(!(months %in% ignore_month))
+  selected_data <- filter_ignoring_months(predictions_df, ignore_month)
   return(max(selected_data$ucl))
 }
 
 get_min <- function(predictions_df, ignore_month = NULL) {
-  selected_data <- predictions_df %>% filter(!(months %in% ignore_month))
+  selected_data <- filter_ignoring_months(predictions_df, ignore_month)
   return(min(selected_data$lcl))
 }
 
 get_median <- function(predictions_df, ignore_month = NULL){
-  selected_data <- predictions_df %>% filter(!(months %in% ignore_month))
+  selected_data <- filter_ignoring_months(predictions_df, ignore_month)
   return(median(selected_data$N))
+}
+
+filter_ignoring_months <- function(predictions_df, ignore_month) {
+  predictions_df %>% filter(!(months %in% ignore_month))
 }
