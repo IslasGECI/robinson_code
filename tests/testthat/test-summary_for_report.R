115c995df6e735475c21501a77badb1dfa2711d9
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-01-11 13:57:37 -0800

    🔇 Silence message

--- tests/testthat/test-summary_for_report.R
+++ tests/testthat/test-summary_for_report.R
@@ -1,5 +1,5 @@
 testthat::describe("Obtain relevant numbers for json", {
-  predictions_df <- read_csv("../data/prediction_with_count_cells_coatis.csv")
+  predictions_df <- read_csv("../data/prediction_with_count_cells_coatis.csv", show_col_types = FALSE)
   it("Obtain prediction for July 2022", {
     month <- "July 2022"
     obtained_prediction <- get_prediction(predictions_df, month)
