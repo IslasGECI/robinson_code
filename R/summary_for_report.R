1323a025d8163224d70d3fd4ddcb2b4fe0817836
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-01-11 12:43:58 -0800

    ♻️  Extract

--- R/summary_for_report.R
+++ R/summary_for_report.R
@@ -4,20 +4,23 @@ get_prediction <- function(predictions_df, month) {
 }
 
 get_max <- function(predictions_df, ignore_month = NULL) {
-  selected_data <- ignoring_months(predictions_df, ignore_month)
-  return(max(selected_data$ucl))
+  get_statistic(predictions_df, ignore_month, `ucl`, max)
 }
 
 get_min <- function(predictions_df, ignore_month = NULL) {
-  selected_data <- ignoring_months(predictions_df, ignore_month)
-  return(min(selected_data$lcl))
+  get_statistic(predictions_df, ignore_month, `lcl`, min)
 }
 
-get_median <- function(predictions_df, ignore_month = NULL){
-  selected_data <- ignoring_months(predictions_df, ignore_month)
-  return(median(selected_data$N))
+get_median <- function(predictions_df, ignore_month = NULL) {
+  get_statistic(predictions_df, ignore_month, `N`, median)
 }
 
 ignoring_months <- function(predictions_df, ignore_month) {
   predictions_df %>% filter(!(months %in% ignore_month))
+}
+
+get_statistic <- function(data, ignore_month, column, statistic) {
+  selected_data <- ignoring_months(data, ignore_month) %>%
+    summarize(computed_statistics = statistic({{ column }}))
+  return(selected_data$computed_statistics)
 }
