bce315116c52941fe3ded99151c4bd443fd57786
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-02-17 14:49:21 -0800

    ✅ Update package version in tests

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,6 +1,6 @@
 testthat::describe("Get version of the module", {
-  it("The version is 0.1.0", {
-    expected_version <- c("0.1.0")
+  it("The version is 1.0.0", {
+    expected_version <- c("1.0.0")
     obtained_version <- packageVersion("robinson")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
