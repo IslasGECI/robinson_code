34706fcd62c7e890d6db1e659e1f336254406262
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-13 11:51:00 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-summary_for_report.R
+++ tests/testthat/test-summary_for_report.R
@@ -1,8 +1,5 @@
 predictions_df <- read_csv("../data/prediction_with_count_cells_coatis.csv", show_col_types = FALSE)
 testthat::describe("Obtain relevant numbers for json", {
-  it("obtains data reception date", {
-    obtained_date <- get_data_reception_date(specie)
-  })
   it("Obtain prediction for July 2022", {
     month <- "July 2022"
     obtained_prediction <- get_prediction(predictions_df, month)
