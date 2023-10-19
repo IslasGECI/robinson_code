bccf73dc849b7adb0151ced7fe6634fe01159f4a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-19 14:45:10 -0700

    ♻️  Refactor

--- R/summary_for_report.R
+++ R/summary_for_report.R
@@ -1,7 +1,3 @@
-get_max <- function(predictions_df, ignore_month = NULL) {
-  get_statistic(predictions_df, ignore_month, `ucl`, max)
-}
-
 get_min <- function(predictions_df, ignore_month = NULL) {
   get_statistic(predictions_df, ignore_month, `lcl`, min)
 }
@@ -56,7 +52,7 @@ Configurator_summary_by_species <- R6::R6Class("Configurator summary by species"
       prediction_date <- private$get_prediction_date(ignore_month)
       list(
         "prediction" = self$get_prediction(prediction_date),
-        "max" = private$xxget_max(ignore_month),
+        "max" = private$get_max(ignore_month),
         "min" = private$xxget_min(ignore_month),
         "median" = private$xxget_median(ignore_month),
         "start_date" = self$get_start_date(),
@@ -105,7 +101,7 @@ Configurator_summary_by_species <- R6::R6Class("Configurator summary by species"
         last()
       return(last_month)
     },
-    xxget_max = function(ignore_month = NULL) {
+    get_max = function(ignore_month = NULL) {
       private$xxget_statistic(self$predictions_df, ignore_month, `ucl`, max)
     },
     xxget_min = function(ignore_month = NULL) {
